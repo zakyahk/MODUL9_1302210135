7efeb866e28e6bce490e685fc2784642a8da4db8
zakyahk <zakyahk@icloud.com>
2023-04-18 16:57:04 +0700

    menambahkan API Mahasiswa

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +29,80 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+namespace MODUL9_1302210135
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        // Data dummy untuk contoh
+        private static List<Mahasiswa> _mahasiswaData = new List<Mahasiswa>()
+        {
+            new Mahasiswa("Zakkiya Hakeem", "1302210135", "Konstruksi Perangkat Lunak" , "1" , 2019),
+            new Mahasiswa("Hafid Naoya", "1302210028", "Konstruksi Perangkat Lunak" , "2" , 2019)
+       
+        };
+
+        public string nim { get; private set; }
+
+        // GET api/mahasiswa
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> GetMahasiswa()
+        {
+            return _mahasiswaData;
+        }
+
+        // GET api/mahasiswa/1302210135
+        [HttpGet("{id}")]
+        public ActionResult<Mahasiswa> GetMahasiswaByNim(int id)
+        {
+            var mahasiswa = _mahasiswaData.FirstOrDefault(m => m.Nim == nim);
+            if (mahasiswa == null)
+            {
+                return NotFound();
+            }
+            return mahasiswa;
+        }
+
+        // POST api/mahasiswa
+        [HttpPost]
+        public ActionResult<Mahasiswa> AddMahasiswa(Mahasiswa mahasiswa)
+        {
+            _mahasiswaData.Add(mahasiswa);
+            return Ok(_mahasiswaData);//CreatedAtAction(nameof(GetMahasiswaByNim), new { nim = mahasiswa.Nim }, mahasiswa);
+        }
+
+        // DELETE api/mahasiswa/1302210135
+        [HttpDelete("{id}")]
+        public ActionResult DeleteMahasiswa(int id)
+        {
+            var existingMahasiswa = _mahasiswaData.FirstOrDefault(m => m.Nim == nim);
+            if (existingMahasiswa == null)
+            {
+                return NotFound();
+            }
+            _mahasiswaData.Remove(existingMahasiswa);
+            return NoContent();
+        }
+    }
+
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+        public string Course { get; internal set; }
+        public string id { get; internal set; }
+        public int Year { get; internal set; }
+        public Mahasiswa(string Nama, string Nim, string Course, string id, int Year)
+        {
+            this.Nama = Nama;
+            this.Nim = Nim;
+            this.Course = Course;
+            this.id = id;
+            this.Year = Year;
+        }
+    }
+
+}
+
